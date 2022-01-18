08f2b207c2529df2c2f1997a5b8eb65cdca55108
Pavel Brichkin <pavlikbrichkin11@gmail.com>
2022-01-19 00:51:38 +0500

    added functionality for add button, added minus button without finctionality, added field for desired item

--- app/src/main/java/com/example/mysecondprojectmoneybox/MainActivity.java
+++ app/src/main/java/com/example/mysecondprojectmoneybox/MainActivity.java
@@ -7,22 +7,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView moneyQuantity = (TextView) findViewById(R.id.money);
-    EditText moneyAddition = (EditText) findViewById(R.id.editText);
+    float money = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView moneyQuantity = (TextView) findViewById(R.id.money);
+        EditText moneyAddition = (EditText) findViewById(R.id.editText);
+        moneyQuantity.setText(Float.toString(money));
         Button addButton = (Button) findViewById(R.id.button);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (moneyAddition.getText().toString().length() != 0) {
+                    money += Float.parseFloat(moneyAddition.getText().toString());
+                    moneyQuantity.setText(Float.toString(money));
+                } else Toast.makeText(getApplicationContext(), R.string.toastAdd, Toast.LENGTH_SHORT).show();  //TODO: добавить музыку после добавления денег
             }
         });
     }
