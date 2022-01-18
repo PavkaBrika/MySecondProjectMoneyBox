189d4160e02b1d0b17d14ce932eb0c161cf5bbde
Pavel Brichkin <pavlikbrichkin11@gmail.com>
2022-01-18 21:22:06 +0500

    started adding functionality

--- app/src/main/java/com/example/mysecondprojectmoneybox/MainActivity.java
+++ app/src/main/java/com/example/mysecondprojectmoneybox/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.mysecondprojectmoneybox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView moneyQuantity = (TextView) findViewById(R.id.money);
+    EditText moneyAddition = (EditText) findViewById(R.id.editText);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addButton = (Button) findViewById(R.id.button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
