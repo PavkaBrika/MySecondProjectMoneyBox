b84c833e332fe1f5b8b8d9845af41d1312d1764b
Pavel Brichkin <pavlikbrichkin11@gmail.com>
2022-01-19 02:00:01 +0500

    now app remembers all info. added new button reset and functionality for it

--- app/src/main/java/com/example/mysecondprojectmoneybox/MainActivity.java
+++ app/src/main/java/com/example/mysecondprojectmoneybox/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.mysecondprojectmoneybox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,25 +13,101 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    float money = 0;
+    private SharedPreferences AppSettings;
+
+    float money;
+    String item;
+
+    TextView moneyQuantity;
+    EditText itemDesire;
+
+    public static final String APP_PREFERENCES = "settings";
+    public static final String APP_PREFERENCES_ITEM = "item";
+    public static final String APP_PREFERENCES_MONEY = "money";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView moneyQuantity = (TextView) findViewById(R.id.money);
+        AppSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+
+        moneyQuantity = (TextView) findViewById(R.id.money);
+
         EditText moneyAddition = (EditText) findViewById(R.id.editText);
+
         moneyQuantity.setText(Float.toString(money));
-        Button addButton = (Button) findViewById(R.id.button);
+
+        Button addButton = (Button) findViewById(R.id.buttonPlus);
+
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (moneyAddition.getText().toString().length() != 0) {
-                    money += Float.parseFloat(moneyAddition.getText().toString());
-                    moneyQuantity.setText(Float.toString(money));
+                    money += Float.parseFloat(moneyAddition.getText().toString()); //TODO: добавить запоминание информации
+                    moneyQuantity.setText(Float.toString(money)); //TODO: добавить выскавиващее окошко ввода при нажатии на кнопку добавить или убавить вместо поля ввода
                 } else Toast.makeText(getApplicationContext(), R.string.toastAdd, Toast.LENGTH_SHORT).show();  //TODO: добавить музыку после добавления денег
             }
         });
+
+        Button subButton = (Button) findViewById(R.id.buttonMinus);
+        subButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    if (moneyAddition.getText().toString().length() != 0) {
+                        money -= Float.parseFloat(moneyAddition.getText().toString());
+                        if (money<0)
+                            money = 0;
+                        moneyQuantity.setText(Float.toString(money));
+                    } else Toast.makeText(getApplicationContext(), R.string.toastAdd, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button resButton = (Button) findViewById(R.id.buttonReset);
+        resButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                money = 0;
+                item = "";
+
+                SharedPreferences.Editor editor = AppSettings.edit();
+                editor.putFloat(APP_PREFERENCES_MONEY, money);
+                editor.putString(APP_PREFERENCES_ITEM, item);
+                editor.apply();
+
+                moneyQuantity = (TextView) findViewById(R.id.money);
+                moneyQuantity.setText(Float.toString(money));
+
+                itemDesire = (EditText) findViewById(R.id.item);
+                itemDesire.setText(item);
+            }
+        });
+    }
+
+    protected void onPause() {
+        super.onPause();
+
+        itemDesire = findViewById(R.id.item);
+        item = itemDesire.getText().toString();
+
+        SharedPreferences.Editor editor = AppSettings.edit();
+        editor.putFloat(APP_PREFERENCES_MONEY, money);
+        editor.putString(APP_PREFERENCES_ITEM, item);
+        editor.apply();
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        if ((AppSettings.contains(APP_PREFERENCES_MONEY)) && (AppSettings.contains(APP_PREFERENCES_ITEM))) {
+            money = AppSettings.getFloat(APP_PREFERENCES_MONEY, 0);
+            moneyQuantity = (TextView) findViewById(R.id.money);
+            moneyQuantity.setText(Float.toString(money));
+
+            item = AppSettings.getString(APP_PREFERENCES_ITEM, "");
+            itemDesire = (EditText) findViewById(R.id.item);
+            itemDesire.setText(item);
+        }
+
     }
 }
