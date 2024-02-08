e0e01c05f5a31123768d18e24b0ec45bb5053baf
PavkaBrika <pavlikbrichkin11@gmail.com>
2024-02-08 12:58:44 +0500

    migrated from appodeal to yandex advertisement

--- app/src/main/java/com/breackneck/mysecondprojectmoneybox/presentation/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/breackneck/mysecondprojectmoneybox/presentation/viewmodel/MainActivityViewModel.kt
@@ -1,6 +1,7 @@
 package com.breackneck.mysecondprojectmoneybox.presentation.viewmodel
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -8,6 +9,8 @@ import com.breckneck.mysecondprojectmoneybox.domain.model.GoalDomain
 import com.breckneck.mysecondprojectmoneybox.domain.usecase.CheckGoalUseCase
 import com.breckneck.mysecondprojectmoneybox.domain.usecase.GetGoalUseCase
 import com.breckneck.mysecondprojectmoneybox.domain.usecase.SetLastShowGoalIdUseCase
+import com.breckneck.mysecondprojectmoneybox.domain.usecase.ads.GetButtonClicksQuantityUseCase
+import com.breckneck.mysecondprojectmoneybox.domain.usecase.ads.SetButtonClickQuantityUseCase
 import com.breckneck.mysecondprojectmoneybox.domain.usecase.settings.GetCharacterUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -16,18 +19,30 @@ class MainActivityViewModel(
     private val getGoalUseCase: GetGoalUseCase,
     private val checkGoalUseCase: CheckGoalUseCase,
     private val getCharacterUseCase: GetCharacterUseCase,
-    private val setLastShowGoalIdUseCase: SetLastShowGoalIdUseCase
+    private val setLastShowGoalIdUseCase: SetLastShowGoalIdUseCase,
+    private val getButtonClickQuantityUseCase: GetButtonClicksQuantityUseCase,
+    private val setButtonClicksQuantityUseCase: SetButtonClickQuantityUseCase
 ): ViewModel() {
 
-    val resultGoal = MutableLiveData<GoalDomain>()
-    val resultCharacter = MutableLiveData<Int>()
+    private val _resultGoal = MutableLiveData<GoalDomain>()
+    val resultGoal: LiveData<GoalDomain>
+        get() = _resultGoal
+    private val _resultCharacter = MutableLiveData<Int>()
+    val resultCharacter: LiveData<Int>
+        get() = _resultCharacter
+    private val _actionClickQuantity = MutableLiveData<Int>()
+    val actionClickQuantity: LiveData<Int>
+        get() = _actionClickQuantity
+
 
     init {
         Log.e("TAG", "VM created")
+        getActionClickQuantity()
     }
 
     override fun onCleared() {
         Log.e("TAG", "VM cleared")
+        setButtonClicksQuantityUseCase.execute(actionClickQuantity.value!!)
         super.onCleared()
     }
 
@@ -40,15 +55,26 @@ class MainActivityViewModel(
                 goal = GoalDomain(id = 1, cost = 0.0, money = 0.0, item = "")
             }
             launch(Dispatchers.Main) {
-                resultGoal.value = goal
+                _resultGoal.value = goal
                 Log.e("TAG", "goal loaded in VM")
             }
         }
     }
 
     fun getCharacter() {
-        resultCharacter.value = getCharacterUseCase.execute()
+        _resultCharacter.value = getCharacterUseCase.execute()
         Log.e("TAG", "Character loaded in VM")
     }
 
+    private fun getActionClickQuantity() {
+        _actionClickQuantity.value = getButtonClickQuantityUseCase.execute()
+    }
+
+    fun incrementActionClickQuantity() {
+        _actionClickQuantity.value = _actionClickQuantity.value!! + 1
+    }
+
+    fun refreshActionClickQuantity() {
+        _actionClickQuantity.value = 0
+    }
 }
---
+++ domain/src/main/java/com/breckneck/mysecondprojectmoneybox/domain/util/Const.kt
@@ -0,0 +1,3 @@
+package com.breckneck.mysecondprojectmoneybox.domain.util
+
+const val CLICKS_QUANTITY_FOR_AD_SHOW = 12
